76b282020f4549a536f5df949ad1f9db7fee435d
Diogo Perillo <diogo@mainframe.com>
2018-09-04 07:44:03 -0300

    Add a few addons

--- demo/.storybook/config.js
+++ demo/.storybook/config.js
@@ -1,8 +1,23 @@
 import { configure } from '@storybook/react'
 
+import { setOptions } from '@storybook/addon-options'
+
+setOptions({
+  name: 'Morpheus UI',
+  url: 'https://github.com/MainframeHQ/morpheus-ui',
+  goFullScreen: false,
+  showAddonsPanel: true,
+  showSearchBox: false,
+  addonPanelInRight: false,
+  sortStoriesByKind: false,
+  hierarchySeparator: /\./,
+  hierarchyRootSeparator: /\|/,
+  enableShortcuts: true,
+})
+
 function loadStories() {
-	require('../stories/index.js')
-	// You can require as many stories as you need.
+  require('../stories/index.js')
+  // You can require as many stories as you need.
 }
 
 configure(loadStories, module)
