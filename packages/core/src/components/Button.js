335f2d73b9ac5b08d9ef27c3380e0438d8524f55
Megan Jezewski <megan@mainframe.com>
2018-09-06 12:07:59 -0600

    merge

--- packages/core/src/components/Button.js
+++ packages/core/src/components/Button.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React, { Component } from 'react'
-<<<<<<< HEAD
 import { TouchableOpacity, Text, StyleSheet } from 'react-native-web'
 
 type Props = {
@@ -83,14 +82,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 })
-=======
-import { Button as RNWButtom } from 'react-native-web'
-
-type Props = {}
-
-export default class Button extends Component<Props> {
-  render() {
-    return <RNWButtom {...this.props} />
-  }
-}
->>>>>>> 4079fb18c4d298c15d4df89cbf10a267722fe748
