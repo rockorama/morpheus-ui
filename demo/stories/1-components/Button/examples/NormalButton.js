119ea2d00434ebbcfca155ebda32fd539e6e706c
Megan Jezewski <megan@mainframe.com>
2018-09-13 09:37:24 -0600

    action

--- demo/stories/1-components/Button/examples/NormalButton.js
+++ demo/stories/1-components/Button/examples/NormalButton.js
@@ -3,6 +3,7 @@
  */
 import React from 'react'
 import { View } from 'react-native'
+import { action } from '@storybook/addon-actions'
 import { Button } from '@morpheus-ui/core'
 
 class NormalButtonExample extends React.Component {
