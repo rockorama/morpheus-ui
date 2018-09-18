4f0dee6af6f711c969a3f9de03100815acae466f
Diogo Perillo <diogo@mainframe.com>
2018-09-18 12:01:26 -0300

    Add ship:demo

--- demo/.storybook/config.js
+++ demo/.storybook/config.js
@@ -12,6 +12,7 @@ addDecorator(centered)
 
 setOptions({
   name: 'Morpheus-UI',
+  title: 'Morpheus-UI',
   url: 'https://github.com/MainframeHQ/morpheus-ui',
   goFullScreen: false,
   addonPanelInRight: false,
