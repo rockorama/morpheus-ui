b75d4fbdf3d281f11a5f80fc131a6d8ebe0bc21d
Diogo Perillo <diogo@mainframe.com>
2018-12-05 19:48:39 -0200

    Button

--- demo/stories/1-components/Button/examples/NormalButton.js
+++ demo/stories/1-components/Button/examples/NormalButton.js
@@ -3,13 +3,46 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { action } from '@storybook/addon-actions'
-import { Button } from '@morpheus-ui/core'
+import { Button, Row, Column } from '@morpheus-ui/core'
+import { CircleArrowRight, User, UserFilled } from '@morpheus-ui/icons'
 
 export default class NormalButtonExample extends Component<{}> {
   render() {
     return (
       <View>
-        <Button onPress={action('clicked')} title="normal button" />
+        <Row size={2}>
+          <Column>
+            <Button
+              onPress={action('clicked')}
+              title="NORMAL BUTTON"
+              Icon={CircleArrowRight}
+              // backgroundColor="yellow"
+              // backgroundHoverColor="red"
+              iconPosition="right"
+              // titleColor="red"
+              // titleHoverColor="blue"
+              // iconColor="red"
+              // iconHoverColor="blue"
+              // noBorder
+              // borderColor="red"
+              // borderHoverColor="blue"
+              // hoverShadow
+            />
+          </Column>
+
+          <Column>
+            <Button
+              onPress={action('clicked')}
+              title="NORMAL BUTTON"
+              Icon={User}
+              HoverIcon={UserFilled}
+              iconSize={{ width: 60, height: 60 }}
+              iconPosition="top"
+              iconColor="red"
+              titleHoverColor="red"
+            />
+          </Column>
+        </Row>
       </View>
     )
   }
--- packages/core/src/components/Button.js
+++ packages/core/src/components/Button.js
@@ -1,87 +1,265 @@
-// @flow
+//@flow
 
-import React, { Component, type Node } from 'react'
-import { Text } from 'react-native'
-import { css } from 'styled-components/native'
-import { turnIntoField } from '@morpheus-ui/forms'
+import React, { Component } from 'react'
+import styled, { css } from 'styled-components/native'
+
+import { turnIntoField, type FieldProps } from '@morpheus-ui/forms'
 import Theme from '../theme'
 
-import HoverView from './HoverView'
-
-type ContainerProps = {
-  submit?: boolean,
-  disabled?: boolean,
-  outlined?: boolean,
-  onPress?: () => {},
-  submitForm?: () => {},
-}
-
-type Props = ContainerProps & {
-  children?: Node,
+type Props = FieldProps & {
   title?: string,
+  Icon?: any,
+  HoverIcon?: any,
+  iconSize: { width: number, height: number },
+  backgroundColor?: string,
+  backgroundHoverColor?: string,
+  iconPosition?: 'top' | 'right' | 'left' | 'bottom',
+  titleColor?: string,
+  titleHoverColor?: string,
+  iconColor?: string,
+  iconHoverColor?: string,
+  borderColor?: string,
+  borderHoverColor?: string,
+  shadow?: boolean,
+  hoverShadow?: boolean,
+  noBorder?: boolean,
+  submit?: boolean,
+
+  onPress?: () => void,
 }
 
-const mainStyle = css`
-  cursor: pointer;
-  border-radius: 23px;
-  padding: 10px;
-  background-color: #da1157;
-  color: white;
-  font-size: 13pt;
-  border: 0;
-  text-align: center;
-  border: 2px solid #da1157;
+type State = {
+  isHover: boolean,
+}
+
+const Container = styled.View`
+  align-items: center;
+  flex-direction: row;
+`
+
+const Clicker = styled.TouchableWithoutFeedback``
+
+const InnerContainer = styled.View`
   transition: all 0.3s;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  padding: ${props => props.muitheme.spacing.basic}px;
+  border-radius: ${props => props.muitheme.borders.roundness}px;
+
   ${props =>
-    props.outlined &&
+    props.backgroundColor &&
     css`
-      background-color: transparent;
-      color: #da1157;
-      border: 2px solid #da1157;
+      background-color: ${props.backgroundColor};
     `};
+
   ${props =>
-    props.disabled &&
+    !props.noBorder &&
     css`
-      background-color: #ffc1dc;
-      border-color: #ffc1dc;
-      cursor: auto;
+      border-color: ${props =>
+        props.borderColor || props.muitheme.colors.main.border};
+      border-width: ${props => props.muitheme.borders.width}px;
+    `};
+
+  ${props =>
+    (props.iconPosition === 'top' || props.iconPosition === 'bottom') &&
+    css`
+      flex-direction: column;
+    `};
+
+  ${props =>
+    props.shadow &&
+    css`
+        shadow-color: #000;
+        shadow-offset: {width: 0, height: 0};
+        shadow-opacity: 0.1;
+        shadow-radius: 8;
+        `};
+
+  ${props => props.containerStyles};
+
+  ${props =>
+    props.isHover &&
+    css`
+      ${props =>
+        !props.noBorder &&
+        css`
+          border-color: ${props =>
+            props.borderHoverColor ||
+            props.borderColor ||
+            props.muitheme.colors.main.border};
+        `};
+
+      ${props =>
+        props.hoverShadow &&
+        css`
+          shadow-color: #000;
+          shadow-offset: {width: 0, height: 0};
+          shadow-opacity: 0.1;
+          shadow-radius: 8;
+          `};
+
+      ${props =>
+        props.backgroundHoverColor &&
+        css`
+          background-color: ${props.backgroundHoverColor ||
+            props.backgroundColor ||
+            'transparent'};
+        `};
+
+      ${props => props.containerHoverStyles};
     `};
 `
 
-const hoverStyle = css`
-  background-color: #fff;
-  color: #da1157;
-  transition: all 0.2s;
+const IconContainer = styled.View`
+  transition: all 0.3s
+  align-items: center;
+  justify-content: center;
   ${props =>
-    props.outlined &&
+    props.iconColor &&
     css`
-      background-color: #da1157;
-      color: #fff;
-      border: 2px solid #da1157;
+      color: ${props.iconColor};
+    `};
+
+  ${props =>
+    !!props.title &&
+    props.iconPosition === 'top' &&
+    css`
+      margin-bottom: 10px;
+    `};
+
+  ${props =>
+    !!props.title &&
+    props.iconPosition === 'right' &&
+    css`
+      margin-left: 20px;
+    `};
+
+  ${props =>
+    !!props.title &&
+    props.iconPosition === 'bottom' &&
+    css`
+      margin-top: 10px;
+    `};
+
+  ${props =>
+    !!props.title &&
+    props.iconPosition === 'left' &&
+    css`
+      margin-right: 20px;
+    `};
+
+  ${props =>
+    props.isHover &&
+    css`
+      ${props =>
+        (props.iconHoverColor || props.iconColor) &&
+        css`
+          color: ${props.iconHoverColor || props.iconColor};
+        `};
+      ${props => props.iconHoverStyles};
     `};
 `
 
-export class Button extends Component<Props> {
+const Title = styled.Text`
+  font-family: 'Muli';
+  font-size: ${props => props.muitheme.typography.fontSize - 2}px;
+  color: #a9a9a9;
+  transition: all 0.3s;
+  font-weight: bold;
+  color: ${props => props.titleColor || props.muitheme.colors.main.text};
+  ${props => props.titleStyles};
+
+  ${props =>
+    props.isHover &&
+    css`
+      color: ${props =>
+        props.titleHoverColor ||
+        props.titleColor ||
+        props.muitheme.colors.main.activeText};
+      ${props => props.titleHoverStyles};
+    `};
+`
+export class Button extends Component<Props, State> {
   static contextType = Theme
 
-  onPress = () => {
-    if (this.props.submit) {
-      this.props.submitForm && this.props.submitForm()
+  static defaultProps = {
+    iconPosition: 'right',
+    iconSize: {
+      width: 24,
+      height: 24,
+    },
+    titleColor: '#979797',
+    titleHoverColor: '#DA1157',
+    iconColor: '#979797',
+    iconHoverColor: '#DA1157',
+    borderColor: '#979797',
+    borderHoverColor: '#DA1157',
+  }
+
+  state = {
+    isHover: false,
+  }
+
+  onMouseOver = () => {
+    this.setState({
+      isHover: true,
+    })
+  }
+
+  onMouseLeave = () => {
+    this.setState({
+      isHover: false,
+    })
+  }
+
+  onSubmit = () => {
+    if (this.props.inForm && this.props.submit) {
+      this.props.submitForm()
     }
     this.props.onPress && this.props.onPress()
   }
 
-  render() {
-    const { children, title, disabled, ...other } = this.props
+  renderIcon() {
+    const { Icon, HoverIcon } = this.props
+    if (!Icon) return null
+
+    const TheIcon = this.state.isHover ? HoverIcon || Icon : Icon
     return (
-      <HoverView
-        styles={mainStyle}
-        hoverStyles={disabled ? null : hoverStyle}
-        onClick={disabled ? null : this.onPress}
-        disabled={disabled}
-        {...other}>
-        {children ? children : <Text>{title}</Text>}
-      </HoverView>
+      <IconContainer {...this.props} isHover={this.state.isHover}>
+        <TheIcon
+          width={this.props.iconSize.width}
+          height={this.props.iconSize.height}
+        />
+      </IconContainer>
+    )
+  }
+
+  render() {
+    const { title, iconPosition } = this.props
+
+    return (
+      <Container>
+        <Clicker onPress={this.onSubmit}>
+          <InnerContainer
+            {...this.props}
+            onMouseOver={this.onMouseOver}
+            onMouseLeave={this.onMouseLeave}
+            muitheme={this.context}
+            isHover={this.state.isHover}>
+            {(iconPosition === 'left' || iconPosition === 'top') &&
+              this.renderIcon()}
+            <Title
+              {...this.props}
+              isHover={this.state.isHover}
+              muitheme={this.context}>
+              {title}
+            </Title>
+            {(iconPosition === 'right' || iconPosition === 'bottom') &&
+              this.renderIcon()}
+          </InnerContainer>
+        </Clicker>
+      </Container>
     )
   }
 }
